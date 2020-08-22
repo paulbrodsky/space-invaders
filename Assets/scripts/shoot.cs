5f8941c36bb986a3093a040a9dfdb75e98fea4e1
Ben Brodsky <b3nb0b@gmail.com>
2020-08-21 21:49:18 -0400

    init

---
+++ Assets/scripts/shoot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    public GameObject bullet;
+
+    public float speed = 0.1f;
+
+    void Update()
+    {
+        transform.Translate(0, speed, 0);
+    }
+}
