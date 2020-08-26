5f6bdc865f4a1c1e1aa707081aa81114500b2fb5
Ben Brodsky <b3nb0b@gmail.com>
2020-08-25 21:51:12 -0400

    basic movement

---
+++ Assets/scripts/alienmovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alienmovement : MonoBehaviour
+{
+    public float speed = 1000f;
+
+    public Transform alien;
+
+    public float waitTime = 1f;
+
+    public Vector3 location;
+
+    public Transform movePoint;
+
+    public bool move;
+
+
+    void Start()
+    {
+
+
+    }
+    void Update()
+    {
+        waitTime++;
+
+        if (waitTime == 100f)
+        {
+            transform.Translate(Vector3.right);
+
+            waitTime = 1f;
+        }
+    }
+}
